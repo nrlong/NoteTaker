09d097bc39806afc8a3289fbd43eceb11ea8ae5c
Nathan Long <nrlong13@gmail.com>
2020-04-12 17:57:09 -0400

    fixing error for heroku launch

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = process.env.PORT | 3000; 
+const PORT = process.env.PORT || 3000; 
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
